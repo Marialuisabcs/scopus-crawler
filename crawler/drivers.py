610537c58393e4a810761e7d7f1ee1de26155f87
unknown <maria.lbcs@icloud.com>
2021-11-23 19:26:10 -0400

    feat: adds option for executable geckodriver

--- crawler/drivers.py
+++ crawler/drivers.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from selenium import webdriver
 import geckodriver_autoinstaller
@@ -7,13 +8,14 @@ import geckodriver_autoinstaller
 class Drivers:
     @staticmethod
     def firefox(download_dir: str) -> webdriver.Firefox:
-        geckodriver_autoinstaller.install()
+        # geckodriver_autoinstaller.install()
         profile = webdriver.FirefoxProfile()
         profile.set_preference('browser.download.folderList', 2)
         profile.set_preference('browser.download.manager.showWhenStarting', False)
         profile.set_preference('browser.download.dir', download_dir)
         profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
 
-        driver = webdriver.Firefox(firefox_profile=profile, service_log_path=os.path.devnull)
+        path = Path.cwd() / 'geckodriver.exe'
+        driver = webdriver.Firefox(firefox_profile=profile, service_log_path=os.path.devnull, executable_path=path)
 
         return driver
