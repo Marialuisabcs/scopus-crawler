37877b9a37b1cd3ccf023038ab61397b9cd7588f
unknown <maria.lbcs@icloud.com>
2021-12-06 11:38:49 -0400

    feat: defines database models

---
+++ models.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from peewee import *
+
+db = SqliteDatabase('local-storage.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Study(BaseModel):
+    id = AutoField()
+    title = CharField()
+    abstract = TextField()
+    eid = CharField(null=True)
+
+
+def create_tables():
+    db.create_tables(Study)
